847313b730f8554a1c94e06f109a22a3d27dc831
mBentaifour <bentaifourmoh@gmail.com>
2024-08-24 17:42:27 -0700

    web stack debugging4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,16 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+# we are getting a huge amount of failed requests
+
+# using ApacheBench which is a quite popular tool in the industry.
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
